13b34c001349b525aa2cda21f34f43e99f9a5b66
emanuelefavero <emafaux89@gmail.com>
2024-04-24 14:50:24 +0200

    add - check if GET request

--- server.js
+++ server.js
@@ -11,21 +11,33 @@ const server = http.createServer((req, res) => {
   console.log(req.method)
 
   // Create a basic router
-  // * /
-  if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    res.end(`<h1>Homepage</h1><a href='/random-number'>Get Random Number</a>`)
-    // TIP: res.write('Hello') can also be used to send a response
+  try {
+    // Check if GET request
+    if (req.method === 'GET') {
+      // * /
+      if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.end(
+          `<h1>Homepage</h1><a href='/random-number'>Get Random Number</a>`
+        )
+        // TIP: res.write('Hello') can also be used to send a response
 
-    // * /random-number
-  } else if (req.url === '/random-number') {
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    res.end(`<h1>Random Number: ${randomNumber}</h1><a href='/'>Home</a>`)
+        // * /random-number
+      } else if (req.url === '/random-number') {
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.end(`<h1>Random Number: ${randomNumber}</h1><a href='/'>Home</a>`)
 
-    // * Not Found
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/html' })
-    res.end(`<h1>Not Found</h1><a href='/'>Home</a>`)
+        // * Not Found
+      } else {
+        res.writeHead(404, { 'Content-Type': 'text/html' })
+        res.end(`<h1>Not Found</h1><a href='/'>Home</a>`)
+      }
+    } else {
+      throw new Error('Method Not Allowed')
+    }
+  } catch (error) {
+    res.writeHead(500, { 'Content-Type': 'text/plain' })
+    res.end(`Server Error: ${error.message}`)
   }
 })
 
