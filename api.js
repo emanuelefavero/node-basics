e1f94071b9386fad5b8373082f24daf7cbf3ef95
emanuelefavero <emafaux89@gmail.com>
2024-04-24 18:59:55 +0200

    add middleware

--- api.js
+++ api.js
@@ -8,39 +8,53 @@ const users = [
   { id: 3, name: 'Gus Fring' },
 ]
 
-const server = createServer((req, res) => {
+// * Logger Middleware
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`)
+  next()
+}
+
+// * JSON Header Middleware
+const jsonHeader = (req, res, next) => {
   res.setHeader('Content-Type', 'application/json')
+  next()
+}
 
-  // * Handle GET /api/users
-  if (req.url === '/api/users' && req.method === 'GET') {
-    res.statusCode = 200
-    res.write(JSON.stringify(users))
-  }
+const server = createServer((req, res) => {
+  logger(req, res, () => {
+    jsonHeader(req, res, () => {
+      // * Handle GET /api/users
+      if (req.url === '/api/users' && req.method === 'GET') {
+        res.statusCode = 200
+        res.write(JSON.stringify(users))
+      }
 
-  // * Handle GET /api/users/:id
-  else if (req.url.match(/\/api\/users\/\d+/) && req.method === 'GET') {
-    const id = req.url.split('/')[3] // get the user id from URL
-    const user = users.find((user) => user.id === parseInt(id))
+      // * Handle GET /api/users/:id
+      else if (req.url.match(/\/api\/users\/\d+/) && req.method === 'GET') {
+        const id = req.url.split('/')[3] // get the user id from URL
+        const user = users.find((user) => user.id === parseInt(id))
 
-    if (user) {
-      res.statusCode = 200
-      res.write(JSON.stringify(user))
-    }
+        if (user) {
+          res.statusCode = 200
+          res.write(JSON.stringify(user))
+        }
 
-    // * Handle User Not Found
-    else {
-      res.statusCode = 404
-      res.write(JSON.stringify({ message: 'User Not Found' }))
-    }
-  }
+        // * Handle User Not Found
+        else {
+          res.statusCode = 404
+          res.write(JSON.stringify({ message: 'User Not Found' }))
+        }
+      }
 
-  // * Handle Route Not Found
-  else {
-    res.statusCode = 404
-    res.write(JSON.stringify({ message: 'Route Not Found' }))
-  }
+      // * Handle Route Not Found
+      else {
+        res.statusCode = 404
+        res.write(JSON.stringify({ message: 'Route Not Found' }))
+      }
 
-  res.end()
+      res.end()
+    })
+  })
 })
 
 const PORT = process.env.PORT || 4000
