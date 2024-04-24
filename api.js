6f8d8d35a7d6d3b5067c09eadef4d5bdd3265e67
emanuelefavero <emafaux89@gmail.com>
2024-04-24 18:38:43 +0200

    implement api

---
+++ api.js
@@ -0,0 +1,50 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+import { createServer } from 'http'
+
+const users = [
+  { id: 1, name: 'Walter White' },
+  { id: 2, name: 'Jesse Pinkman' },
+  { id: 3, name: 'Gus Fring' },
+]
+
+const server = createServer((req, res) => {
+  res.setHeader('Content-Type', 'application/json')
+
+  // * Handle GET /api/users
+  if (req.url === '/api/users' && req.method === 'GET') {
+    res.statusCode = 200
+    res.write(JSON.stringify(users))
+  }
+
+  // * Handle GET /api/users/:id
+  else if (req.url.match(/\/api\/users\/\d+/) && req.method === 'GET') {
+    const id = req.url.split('/')[3] // get the user id from URL
+    const user = users.find((user) => user.id === parseInt(id))
+
+    if (user) {
+      res.statusCode = 200
+      res.write(JSON.stringify(user))
+    }
+
+    // * Handle User Not Found
+    else {
+      res.statusCode = 404
+      res.write(JSON.stringify({ message: 'User Not Found' }))
+    }
+  }
+
+  // * Handle Route Not Found
+  else {
+    res.statusCode = 404
+    res.write(JSON.stringify({ message: 'Route Not Found' }))
+  }
+
+  res.end()
+})
+
+const PORT = process.env.PORT || 4000
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
