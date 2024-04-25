9021df79baf040a56b5993b9f34261f16fdea7a1
emanuelefavero <emafaux89@gmail.com>
2024-04-25 13:43:04 +0200

    add error handling in events.js

--- events.js
+++ events.js
@@ -10,8 +10,18 @@ function goodbyeHandler() {
   console.log('Goodbye')
 }
 
+// Register event listeners
 emitter.on('greet', greetHandler)
 emitter.on('goodbye', goodbyeHandler)
 
+// Emit events
 emitter.emit('greet', 'John') // Hello John
 emitter.emit('goodbye') // Goodbye
+
+// Error handling
+emitter.on('error', (error) => {
+  console.log(error) // <- Error: Something went wrong
+})
+
+// Simulate error - uncomment to see the error message
+emitter.emit('error', new Error('Something went wrong'))
